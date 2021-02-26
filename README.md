10d1dd385e5913a55659f42fb702646fe3e99bc3
Maksym <maximka3d@gmail.com>
2021-02-25 20:27:36 -0600

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Blur
- trying different blur algorithms in webgl
+Real time gaussian blur in webgl.
+ 
+# Optimizations
+ 
+## Vertical blur --> Horizontal blur 
+- doing vertical and horizontal blurs in a sequence is equivalent to a regular blur.
+- texture lookups (for each fragment) are reduced from 9x9 to 9*2.
+- no loss of quality.
+ 
+## Multiple rounds 
+- alternative to increasing the kernel size. 
+- 2 rounds of 9x9 is equivalent to a 36x36 gaussian blur.
+- adds flexibility
+ 
+## Downscaling
+- less pixels --> dramatic increase in computation --> can do more rounds
+- has small overhead.
+- may result in visual artifacts, especially when there is lots of movement.
+ 
+## Linear sampling
+- approximating a larger kernel (e.g 8x8 --> 17x17) by using bilinear filtering.
+- some work is delegated to the hardware.
+
+
+# Resources used:
+* https://rastergrid.com/blog/2010/09/efficient-gaussian-blur-with-linear-sampling/
+* https://webglfundamentals.org/webgl/lessons/webgl-image-processing-continued.html
+* https://venturebeat.com/2017/07/13/an-investigation-of-fast-real-time-gpu-based-image-blur-algorithms/
+* https://gamedev.stackexchange.com/questions/27474/optimizing-gaussian-blur-with-linear-filtering
--- webgl-demo.js
+++ webgl-demo.js
@@ -16,7 +16,7 @@ var rounds_input = document.getElementById('rounds_input');
 var rounds_value = document.getElementById('rounds_value');
 
 rounds_input.oninput = function() {
-  rounds_value.value = this.value;
+    rounds_value.value = this.value;
 };
 
 var downscale_input = document.getElementById('downscale_input');
@@ -24,15 +24,15 @@ var downscale_value = document.getElementById('downscale_value');
 var downscalePrev = downscale_value; // tracking changes, to avoid reinitializing stuff every frame
 
 downscale_input.oninput = function() {
-  downscale_value.value = this.value;
-  textureChanged = true;
+    downscale_value.value = this.value;
+    textureChanged = true;
 };
 
 const fps_field = document.getElementById('fps');
 
 var linearSampling_checkbox = document.getElementById('ls_checkbox');
 linearSampling_checkbox.onchange = function() {
-  textureChanged = true;
+    textureChanged = true;
 }
 
 
@@ -67,7 +67,7 @@ function main() {
     // resolution of the video
 
     video.addEventListener('loadedmetadata', function(e) {
-      processVideo(video); 
+        processVideo(video);
     }, true);
 
     video.src = url;
@@ -107,177 +107,174 @@ function processVideo(video) {
         return;
     }
 
-    console.log(gl.getParameter(gl.VERSION));
-  
+
     // Initialize a shader program
     const vertexSource = document.getElementById("vertex-shader").text;
     const fragmentSource = document.getElementById("fragment-shader").text;
-    shaderProgram = linkShaders(gl, vertexSource, fragmentSource);  
+    shaderProgram = linkShaders(gl, vertexSource, fragmentSource);
 
     // Define attributes & uniform variables 
     // for our shader program
     const attributeLocations = {
-      vertexPosition: gl.getAttribLocation(shaderProgram, 'vPosition'),
+        vertexPosition: gl.getAttribLocation(shaderProgram, 'vPosition'),
     }
 
     const uniformLocations = {
-      videoTexture: gl.getUniformLocation(shaderProgram, 'videoTexture'),
-      pixelSize: gl.getUniformLocation(shaderProgram, 'pixelSize'),
-      stage: gl.getUniformLocation(shaderProgram, 'stage'),
-      flipY: gl.getUniformLocation(shaderProgram, 'flipY'),    
-      offsets: gl.getUniformLocation(shaderProgram, 'offsets'),  
-      weights: gl.getUniformLocation(shaderProgram, 'weights'),  
+        videoTexture: gl.getUniformLocation(shaderProgram, 'videoTexture'),
+        pixelSize: gl.getUniformLocation(shaderProgram, 'pixelSize'),
+        stage: gl.getUniformLocation(shaderProgram, 'stage'),
+        flipY: gl.getUniformLocation(shaderProgram, 'flipY'),
+        offsets: gl.getUniformLocation(shaderProgram, 'offsets'),
+        weights: gl.getUniformLocation(shaderProgram, 'weights'),
     }
-   
+
     // Binding buffers
     const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
-    const indices = [ 0,1,3,0,3,2 ];
-  
+    const indices = [0, 1, 3, 0, 3, 2];
+
     const positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-  
+
     const indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(indices), gl.STATIC_DRAW);
-  
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
     // Tell WebGL how to pull out the positions from the position
     // buffer into the vertexPosition attribute    
     {
-      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-      gl.vertexAttribPointer(attributeLocations.vertexPosition,2,gl.FLOAT,false,0,0);
-      gl.enableVertexAttribArray(attributeLocations.vertexPosition);
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        gl.vertexAttribPointer(attributeLocations.vertexPosition, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(attributeLocations.vertexPosition);
     }
-  
+
     // Tell WebGL which indices to use to index the vertices
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.useProgram(shaderProgram); 
-    
+    gl.useProgram(shaderProgram);
+
     // Make 2 temporary framebuffers to store intermidiate results
     // Explained here: https://webglfundamentals.org/webgl/lessons/webgl-image-processing-continued.html
-        
-    var x = video.videoWidth/downscale_value.value;
-    var y = video.videoHeight/downscale_value.value;
+
+    var x = video.videoWidth / downscale_value.value;
+    var y = video.videoHeight / downscale_value.value;
     var X = video.videoWidth;
     var Y = video.videoHeight;
 
-    var originalTexture = initTexture(gl,X,Y);
+    var originalTexture = initTexture(gl, X, Y);
     var tempTextures = [];
     var framebuffers = [];
 
 
     for (var ii = 0; ii < 2; ++ii) {
-      var texture = initTexture(gl,x,y);
-      tempTextures.push(texture);
-        
-      gl.texImage2D(
-        gl.TEXTURE_2D, 0, gl.RGBA, x, y, 0,
-        gl.RGBA, gl.UNSIGNED_BYTE, null);
-  
-      // create a framebuffer
-      var fbo = gl.createFramebuffer();
-      framebuffers.push(fbo);
-      gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
-  
-      // attach a texture to it.
-      gl.framebufferTexture2D(
-          gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
-    }
-             
+        var texture = initTexture(gl, x, y);
+        tempTextures.push(texture);
 
-    function drawToFrameBuffer(frameBufferID){     
-      gl.uniform1i(uniformLocations.stage, frameBufferID%2==0 ? 0:1);  
-      gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffers[frameBufferID]);    
-      gl.viewport(0, 0, x, y);  
-      gl.uniform2fv(uniformLocations.pixelSize, [radius_value.value*1.0/x, radius_value.value*1.0/y]);
-      gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);     
-      gl.bindTexture(gl.TEXTURE_2D, tempTextures[frameBufferID]);             
+        gl.texImage2D(
+            gl.TEXTURE_2D, 0, gl.RGBA, x, y, 0,
+            gl.RGBA, gl.UNSIGNED_BYTE, null);
+
+        // create a framebuffer
+        var fbo = gl.createFramebuffer();
+        framebuffers.push(fbo);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+
+        // attach a texture to it.
+        gl.framebufferTexture2D(
+            gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
     }
-  
+
+
+    function drawToFrameBuffer(frameBufferID) {
+        gl.uniform1i(uniformLocations.stage, frameBufferID % 2 == 0 ? 0 : 1);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffers[frameBufferID]);
+        gl.viewport(0, 0, x, y);
+        gl.uniform2fv(uniformLocations.pixelSize, [radius_value.value * 1.0 / x, radius_value.value * 1.0 / y]);
+        gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+        gl.bindTexture(gl.TEXTURE_2D, tempTextures[frameBufferID]);
+    }
+
     // These produce different blurs
     var kernels = {
-      linear: {
-        offsets : [0.0, 1.43478, 3.34783, 5.26087, 7.17391],
-        weights : [0.16819, 0.27277, 0.116901, 0.0240679, 0.00211122],
-      },
-      discrete: {
-        offsets : [0.0, 1.0, 2.0, 3.0, 4.0],
-        weights : [0.20236, 0.179044, 0.124009, 0.067234, 0.028532],
-      }
+        linear: {
+            offsets: [0.0, 1.43478, 3.34783, 5.26087, 7.17391],
+            weights: [0.16819, 0.27277, 0.116901, 0.0240679, 0.00211122],
+        },
+        discrete: {
+            offsets: [0.0, 1.0, 2.0, 3.0, 4.0],
+            weights: [0.20236, 0.179044, 0.124009, 0.067234, 0.028532],
+        }
     }
 
     gl.uniform1fv(uniformLocations.offsets, kernels.discrete.offsets);
     gl.uniform1fv(uniformLocations.weights, kernels.discrete.weights);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);  
-     
-    function render() {          
-        if(textureChanged){
-            downscalePrev=downscale_value.value;
-            x = video.videoWidth/downscale_value.value;
-            y = video.videoHeight/downscale_value.value;    
-            gl.bindTexture(gl.TEXTURE_2D, tempTextures[0]);      
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    function render() {
+        if (textureChanged) {
+            downscalePrev = downscale_value.value;
+            x = video.videoWidth / downscale_value.value;
+            y = video.videoHeight / downscale_value.value;
+            gl.bindTexture(gl.TEXTURE_2D, tempTextures[0]);
             gl.texImage2D(
-              gl.TEXTURE_2D, 0, gl.RGBA, x, y, 0,
-              gl.RGBA, gl.UNSIGNED_BYTE, null);
-            gl.bindTexture(gl.TEXTURE_2D, tempTextures[1]);      
-              gl.texImage2D(
                 gl.TEXTURE_2D, 0, gl.RGBA, x, y, 0,
-                gl.RGBA, gl.UNSIGNED_BYTE, null); 
-            if(linearSampling_checkbox.checked){         
+                gl.RGBA, gl.UNSIGNED_BYTE, null);
+            gl.bindTexture(gl.TEXTURE_2D, tempTextures[1]);
+            gl.texImage2D(
+                gl.TEXTURE_2D, 0, gl.RGBA, x, y, 0,
+                gl.RGBA, gl.UNSIGNED_BYTE, null);
+            if (linearSampling_checkbox.checked) {
                 gl.uniform1fv(uniformLocations.offsets, kernels.linear.offsets);
                 gl.uniform1fv(uniformLocations.weights, kernels.linear.weights);
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR); 
-            }
-            else{
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+            } else {
                 gl.uniform1fv(uniformLocations.offsets, kernels.discrete.offsets);
                 gl.uniform1fv(uniformLocations.weights, kernels.discrete.weights);
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST); 
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
             }
             textureChanged = false;
         }
 
-        if (copyVideo){
-    
-          // 1. Update original texture
-          gl.bindTexture(gl.TEXTURE_2D, originalTexture);
-          gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
-        
-          // 2. Ping-ponging data from framebuffer[0] to framebuffer[1] 
-          gl.uniform1i(uniformLocations.flipY, 1);
+        if (copyVideo) {
 
-          var n = (radius_value.value==0) ? 1 : rounds_value.value;
+            // 1. Update original texture
+            gl.bindTexture(gl.TEXTURE_2D, originalTexture);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
 
-          for(var i=1; i<=n; ++i){
-            if(i==n){
-              drawToFrameBuffer(0);
+            // 2. Ping-ponging data from framebuffer[0] to framebuffer[1] 
+            gl.uniform1i(uniformLocations.flipY, 1);
+
+            var n = (radius_value.value == 0) ? 1 : rounds_value.value;
+
+            for (var i = 1; i <= n; ++i) {
+                if (i == n) {
+                    drawToFrameBuffer(0);
+                } else {
+                    drawToFrameBuffer(0);
+                    drawToFrameBuffer(1);
+                }
             }
-            else{
-              drawToFrameBuffer(0);
-              drawToFrameBuffer(1);
-            }
-          }      
 
-          // 3. Drawing to canvas
-          gl.uniform1i(uniformLocations.flipY, -1);
-          gl.uniform1i(uniformLocations.stage, 1);
-          gl.bindFramebuffer(gl.FRAMEBUFFER, null);    
-          gl.viewport(0, 0, X, Y);             
-          gl.uniform2fv(uniformLocations.pixelSize, [radius_value.value*1.0/X, radius_value.value*1.0/Y]);
-          gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);   
-        }        
-        
+            // 3. Drawing to canvas
+            gl.uniform1i(uniformLocations.flipY, -1);
+            gl.uniform1i(uniformLocations.stage, 1);
+            gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+            gl.viewport(0, 0, X, Y);
+            gl.uniform2fv(uniformLocations.pixelSize, [radius_value.value * 1.0 / X, radius_value.value * 1.0 / Y]);
+            gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+        }
+
         // FPS
         var now = new Date().getTime();
         frameCount++;
-        elapsedTime += (now - lastTime);     
-        lastTime = now;     
-        if(elapsedTime >= 1000) {
+        elapsedTime += (now - lastTime);
+        lastTime = now;
+        if (elapsedTime >= 1000) {
             fps = frameCount;
             frameCount = 0;
-            elapsedTime -= 1000;     
+            elapsedTime -= 1000;
             fps_field.innerHTML = fps;
         }
 
@@ -293,20 +290,19 @@ function processVideo(video) {
 //
 // Initialize a texture.
 //
-function initTexture(gl, width, height) {  
+function initTexture(gl, width, height) {
     var texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR); 
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
     gl.texImage2D(
-      gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0,
-      gl.RGBA, gl.UNSIGNED_BYTE, null);
+        gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0,
+        gl.RGBA, gl.UNSIGNED_BYTE, null);
     return texture;
 }
-
 
 
 //
@@ -324,7 +320,7 @@ function linkShaders(gl, vsSource, fsSource) {
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
         alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
         return null;
-    }     
+    }
     return shaderProgram;
 }
 
@@ -333,7 +329,7 @@ function linkShaders(gl, vsSource, fsSource) {
 // creates a shader of the given type, uploads the source and
 // compiles it.
 //
-function loadShader(gl, type, source) {  
+function loadShader(gl, type, source) {
     const shader = gl.createShader(type);
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
